a998a6738264660b44ad318c30142aa8b481def1
namph-uet <hoangnamuet.vnu@gmail.com>
2021-06-16 01:39:59 +0700

    "update relative path"

--- mvc/views/partials/admin-menu.php
+++ mvc/views/partials/admin-menu.php
@@ -3,12 +3,12 @@
 	<div class="bg pt-0" style="width: 100%;">
 		<div class="bg p-2" style="position: fixed;border-radius: 10px;">
 			<div class="mt-2" style="width: 100%;height: 40px;border-bottom: 2px solid #fff;opacity: 0.9">
-				<a href="/shoppet/product/productManage" style="text-decoration: none;">
+				<a href="/Product/productManage" style="text-decoration: none;">
 					<p class="mb-0 font-weight-bold text-white text-center" style="font-size: 120%">Quản lý sản phẩm</p>
 				</a>
 			</div>
 			<div class="mt-2" style="width: 100%;height: 40px;border-bottom: 2px solid #fff;opacity: 0.9">
-				<a href="/shoppet/order/OrderManage" style="text-decoration: none;">
+				<a href="/Order/OrderManage" style="text-decoration: none;">
 					<p class="mb-0 font-weight-bold text-white text-center" style="font-size: 120%">Quản lý đơn hàng</p>
 				</a>
 			</div>
@@ -16,12 +16,12 @@
 				<p class="mb-0 font-weight-bold text-white text-center" style="font-size: 120%">Quản lý hệ thông</p>
 			</div> -->
 			<div class="mt-2" style="width: 100%;height: 40px;border-bottom: 2px solid #fff;opacity: 0.9">
-				<a href="/shoppet/message" style="text-decoration: none;">
+				<a href="/Message" style="text-decoration: none;">
 					<p class="mb-0 font-weight-bold text-white text-center" style="font-size: 120%">Tin nhắn</p>
 				</a>
 			</div>
 			<div class="mt-2" style="width: 100%;height: 40px;border-bottom: 2px solid #fff;opacity: 0.9">
-				<a href="/shoppet/login/logout" style="text-decoration: none;">
+				<a href="/Login/Logout" style="text-decoration: none;">
 					<p class="mb-0 font-weight-bold text-white text-center" style="font-size: 120%">Đăng xuất</p>
 				</a>
 			</div>
